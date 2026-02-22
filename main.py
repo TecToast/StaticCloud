81eed70f0f8292eaaafb57ff091e2a6a49920bbc
Flo <tectoast@gmail.com>
2026-02-22 21:18:39 +0100

    feat: add hash_length form parameter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 import hashlib
-from fastapi import FastAPI, UploadFile, File, HTTPException, Header
+from fastapi import FastAPI, UploadFile, File, HTTPException, Header, Form
 from fastapi.staticfiles import StaticFiles
 
 UPLOAD_DIR = "/images"
@@ -14,7 +14,8 @@ app.mount("/i", StaticFiles(directory=UPLOAD_DIR), name="images")
 @app.post("/upload")
 async def upload_image(
     file: UploadFile = File(...),
-    authorization: str = Header(None)
+    authorization: str = Header(None),
+    hash_length: int = Form(16)
 ):
     if authorization != f"Bearer {UPLOAD_TOKEN}":
         raise HTTPException(status_code=401)
@@ -26,7 +27,7 @@ async def upload_image(
     file_hash = hashlib.sha256()
     file_content = await file.read()
     file_hash.update(file_content)
-    hash_hex = file_hash.hexdigest()[:16]
+    hash_hex = file_hash.hexdigest()[:hash_length]
 
     unique_key = hash_hex + ext
     file_path = os.path.join(UPLOAD_DIR, unique_key)
